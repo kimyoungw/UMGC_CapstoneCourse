148bdf50ff7eb7dc1369eeccae24beaf2d847a2a
kimyoungw <k.youngw@gmail.com>
2022-10-12 16:13:03 -0400

    Update run_readcheck.sh

--- scripts/run_readcheck.sh
+++ scripts/run_readcheck.sh
@@ -87,13 +87,12 @@ bash ${thisDir}/../fastq-info/bin/fastqinfo-2.0.sh -r 125 01_reads_qc_trim/short
 #add a parse of coverage.txt to determine if row 2 column 5 is >10x, if not end script
 
 tail -1 coverage.txt | awk -F"\t" '{print$5}' > coverageout.txt
-#sed 's/...//' coverageout.txt
 coveragevalue=$(cat coverageout.txt)
 if [[ $coveragevalue -le 10 ]]; then
 	echo "Less than 10x coverage"
 	rm coverageout.txt
 	exit 0
-#else
+else
 	mv coverage.txt 01_reads_qc_trim/
 	rm coverageout.txt
 fi
