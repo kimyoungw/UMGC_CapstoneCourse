9268346609fd7a55baff8d279bf3a1db09f1e9fe
kimyoungw <k.youngw@gmail.com>
2022-10-12 15:16:10 -0400

    Update run_varcall.sh

--- scripts/run_varcall.sh
+++ scripts/run_varcall.sh
@@ -1,5 +1,44 @@
 #!/usr/bin/bash
 
+#This script will
+#1) Take the output of run_alignment.sh (marked_duplicates.bam) and
+#2) Generate
+
+#HELP function
+function HELP {
+echo ""
+echo "Usage:" $0
+echo "                  -r path/to/reference/                   reference genome in fasta format"
+echo "                  -i path/to/inputfile/                   input file in .bam format"
+echo ""
+echo "Example: $0 -r /path/to/refgenome.fasta -i /path/to/input.bam "
+echo ""
+exit 0
+}
+
+###Take arguments
+#Run HELP if -h -? or invalid input
+#Set REFERENCE to -r
+#Set INPUTFILE to -i
+
+while getopts ":hr:i:" option; do
+        case ${option} in
+                h)
+                HELP
+                ;;
+                r)
+                export REFERENCE=${OPTARG}
+                ;;
+                i)
+                export INPUTFILE=${OPTARG}
+                ;;
+                \?)
+                echo "Invalid option: ${OPTARG}" 1>&2
+                HELP
+                ;;
+        esac
+done
+
 #Check if all parameters are filled
 if [[ -z "${REFERENCE}" || -z "${INPUTFILE}" ]]; then
         echo ""
@@ -9,18 +48,16 @@ fi
 
 #Run bcftools
 echo ""
-echo "checking for SNPs/indels using samtools/bcftools..."
+echo "Running mpileup and calling SNPs/indels using bcftools..."
 echo ""
-bcftools mpileup -f ${REFERENCE} -d 250 ${INPUTFILE} | bcftools call -m - > bcftools_finaloutput.vcf
+docker run -t --rm -u $(id -u):$(id -g) -v $(pwd):/data:rw -w /data staphb/bcftools bcftools mpileup -Ou -f ${REFERENCE} -d 8000 ${INPUTFILE} | bcftools call -mv -Ob -o calls.vcf
 
-if [ -d 06_variants ]; then
-        echo "06_variants/ already exists"
+if [ -d 03_varcall ]; then
+        echo "03_varcall/ already exists"
         echo ""
 else
-        mkdir -v 06_variants/
+        mkdir -v 03_variants/
 fi
 
 #move vcf files to 06_variants folder
 mv *.vcf 06_variants/
-
-
